fa6dfb4467530750c9494865e9f44f5d19f8d31f
02705534288 <leandronascimento@lampp-it.com.br>
2024-11-04 13:14:38 -0500

    fix: exportando componente main layout

--- src/app/layouts/layouts.module.ts
+++ src/app/layouts/layouts.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MainLayoutComponent
+  ],
+  exports:[
+    MainLayoutComponent
   ]
 })
 export class LayoutsModule { }
