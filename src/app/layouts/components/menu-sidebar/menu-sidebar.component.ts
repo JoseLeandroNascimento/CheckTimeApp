bccd1e2eeede2b27e3305348b2184b9cd162dc44
02705534288 <leandronascimento@lampp-it.com.br>
2024-11-04 15:27:28 -0500

    fix: mudando altura do footer

--- src/app/layouts/components/menu-sidebar/menu-sidebar.component.ts
+++ src/app/layouts/components/menu-sidebar/menu-sidebar.component.ts
@@ -1,12 +1,16 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-menu-sidebar',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './menu-sidebar.component.html',
   styleUrl: './menu-sidebar.component.scss'
 })
 export class MenuSidebarComponent {
 
+  public showMenu:boolean = true
 }
