af86ce2f8f74421f43c117d57ac9bcb271aa14db
02705534288 <leandronascimento@lampp-it.com.br>
2024-11-04 19:22:17 -0500

    feat: create component card

--- src/app/features/employee/pages/cadastro/cadastro.component.ts
+++ src/app/features/employee/pages/cadastro/cadastro.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { EmployeeFormComponent } from '../../components/form/employee-form/employee-form.component';
+import { SharedModule } from '../../../../shared/shared.module';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
   imports: [
-    EmployeeFormComponent
+    EmployeeFormComponent,
+    SharedModule
   ],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
