9964c8b4c236f467687bde7e3cb66856c15cb804
02705534288 <leandronascimento@lampp-it.com.br>
2024-11-07 16:06:14 -0500

    fix: create test menu item

--- src/app/layouts/components/menu-item/menu-item.component.spec.ts
+++ src/app/layouts/components/menu-item/menu-item.component.spec.ts
@@ -1,19 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MenuItemComponent } from './menu-item.component';
+import { IMenuOption } from '../../../shared/interfaces/menu.interface';
+import { ActivatedRoute } from '@angular/router';
 
 describe('MenuItemComponent', () => {
   let component: MenuItemComponent;
   let fixture: ComponentFixture<MenuItemComponent>;
+  let itemMenu: IMenuOption = {label:"test",routerLink:null}
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MenuItemComponent]
+      imports: [MenuItemComponent],
+      providers:[
+        {provide: ActivatedRoute,useValue: {snapshot:{data:{}}}}
+      ]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(MenuItemComponent);
     component = fixture.componentInstance;
+    fixture.componentRef.setInput("itemMenu",itemMenu)
     fixture.detectChanges();
   });
 
