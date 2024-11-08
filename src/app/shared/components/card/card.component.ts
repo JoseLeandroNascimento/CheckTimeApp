95b27f0cf988a393b7c1f133d23f85abc6840e5a
02705534288 <leandronascimento@lampp-it.com.br>
2024-11-07 19:54:04 -0500

    fix: estilizando host do card

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [
+  ],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
+
+  @HostBinding("class") classHost: string = "p-6 w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+
 
 }
